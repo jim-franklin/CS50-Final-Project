c131f2d999afb2bf558a12a7d6ccd708eb49689c
jimFranklin <jimmyfranklin2000@gmail.com>
2023-09-03 20:45:57 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# YOUR PROJECT TITLE
+
+    #### Video Demo:  <URL HERE>
+    #### Description:
+    TODO
---
+++ test_project.py
@@ -0,0 +1,20 @@
+def main():
+    test_function_1()
+    test_function_2()
+    test_function_n()
+
+
+def test_function_1():
+    ...
+
+
+def test_function_2():
+    ...
+
+
+def test_function_n():
+    ...
+
+
+if __name__ == "__main__":
+    main()

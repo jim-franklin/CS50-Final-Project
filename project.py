714f9d92a55a12944311e70e9913ec6f59401da4
jimFranklin <jimmyfranklin2000@gmail.com>
2023-09-04 14:12:23 +0000

    updating project.py

--- project.py
+++ project.py
@@ -1,24 +1,29 @@
+import os
 import sys
 from datetime import date
 import docx
 
 
 def main():
-    if len(sys.argv) == 3:
+    os.chdir(r"C:\Users\HP\PycharmProjects\CS50-Final-Project")
+    if len(sys.argv) == 4:
         create_document()
     else:
         sys.exit(
             """\
         project.py - Generate a Word document that serves as a template for creating formal letters.
         
-        Usage: py.exe project.py letter <company> - Generate template with company's address.
-        py.exe project.py letter Committee - Generate template with standard committee address.
+        Usage: py.exe project.py letter to <company> - Generate template with company's address.
+        py.exe project.py letter to committee - Generate template with standard committee address.
         """
         )
 
 
 def create_document():
-    ...
+    doc_name = f"{sys.argv[1]} {sys.argv[2]} {sys.argv[3]}.docx"
+    doc = docx.Document()
+    doc.add_paragraph(draft())
+    doc.save(doc_name)
 
 
 def date_doc():

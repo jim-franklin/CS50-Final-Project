a7bcf05550e94f26b15aa9a5d75cdd3ea43d92e6
jimFranklin <jimmyfranklin2000@gmail.com>
2023-09-04 11:21:49 +0000

    updating project.py

--- project.py
+++ project.py
@@ -1,17 +1,49 @@
+import sys
+from datetime import date
+
+
 def main():
+    if len(sys.argv) == 1:
+        create_document()
+    else:
+        sys.exit("Usage")
+
+
+def create_document():
     ...
 
 
-def function_1():
+def date_doc():
+    return date.today().strftime("%B %#d, %Y")
+
+
+def address():
     ...
 
 
-def function_2():
-    ...
+def reference():
+    ref_year = str(date.today().year)[2:]
+    if sys.argv[2] == "committee":
+        return "REF:EC/LCLP/COM" + ref_year + "/0.."
+    else:
+        ref_year = str(date.today().year)[2:]
+        return "EC/LCLP/EMO" + ref_year + "/0..."
 
 
-def function_n():
-    ...
+def draft():
+    return "Draft"
+
+
+def greeting():
+    return "Dear Sir/Madam,"
+
+
+def title():
+    return "TITLE:"
+
+
+def complimentary_close():
+    return "Yours faithfully,\n\n" "Ing.Oscar Amonoo-Neizer\n" "(Executive Secretary)"
 
 
 if __name__ == "__main__":

1a6ae964ee5736bf34fdba199c309390fa9f34b0
jimFranklin <jimmyfranklin2000@gmail.com>
2023-09-05 10:00:46 +0000

    updating project.py

--- project.py
+++ project.py
@@ -3,11 +3,17 @@ import sys
 from datetime import date
 from docx import Document
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
+from docx.enum.text import WD_LINE_SPACING
+from docx.shared import Pt
+from docx.shared import Inches
 
 
 def main():
     os.chdir(r"C:\Users\HP\PycharmProjects\CS50-Final-Project")
+    letter = ["letter", "let"]
     if len(sys.argv) == 4:
+        if sys.argv[1].lower() not in letter:
+            raise TypeError(f"\n\n**Did you mean to letter to {sys.argv[3]}?**\n\n")
         create_document()
     else:
         sys.exit(
@@ -21,30 +27,74 @@ def main():
 
 
 def create_document():
-    doc_name = f"{sys.argv[1]}_{sys.argv[2]}_{sys.argv[3]}.docx"
     doc = Document()
 
-    para_draft = doc.add_paragraph(draft())
-    para_draft.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
-    doc.paragraphs[0].runs[0].bold = True
+    # Set custom document style, font, font size and alignment
+    base_style = doc.styles["Normal"]
+    base_style.font.name = "Times New Roman"
+    base_style.font.size = Pt(12)
+    base_style.paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.JUSTIFY
 
-    doc.add_paragraph(reference())
-    doc.paragraphs[1].runs[0].bold = True
+    # Set custom margins
+    section = doc.sections[0]
+    section.left_margin = Inches(0.8)
+    section.right_margin = Inches(0.8)
 
-    para_date = doc.add_paragraph(date_doc())
-    para_date.alignment = WD_PARAGRAPH_ALIGNMENT.RIGHT
+    # Add Draft
+    p_draft = doc.add_paragraph(draft())
+    p_draft.runs[0].bold = True
+    p_draft.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
 
-    doc.add_paragraph(address())
-    doc.paragraphs[3].runs[0].bold = True
+    # Add Reference
+    p_reference = doc.add_paragraph(reference())
+    p_reference.runs[0].bold = True
+    p_reference.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
 
-    doc.add_paragraph(greeting())
+    # Add Date
+    p_date_doc = doc.add_paragraph(date_doc())
+    p_date_doc.alignment = WD_PARAGRAPH_ALIGNMENT.RIGHT
 
-    doc.add_paragraph(title())
-    doc.paragraphs[5].runs[0].underline = True
-    doc.paragraphs[5].runs[0].bold = True
+    # Add Address
+    p_address = doc.add_paragraph(address())
+    p_address.runs[0].bold = True
+    p_address.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
 
-    doc.add_paragraph(complimentary_close())
-    doc.save(doc_name)
+    # Add Greeting or not if the letter is to the committee
+    if sys.argv[3].lower() != "committee":
+        para_greet = doc.add_paragraph(greeting())
+        para_greet.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
+
+    # Add Title
+    p_title = doc.add_paragraph(title())
+    p_title.runs[0].underline = True
+    p_title.runs[0].bold = True
+    p_title.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+
+    # Add body of letter
+    body_body = body()
+    # body of committee letter
+    if sys.argv[3].lower() == "committee":
+        string = body_body.split("\n")
+        for line in string:
+            p_body = doc.add_paragraph(line)
+            p_body.paragraph_format.line_spacing_rule = WD_LINE_SPACING.ONE_POINT_FIVE
+    # body of any other letter
+    else:
+        p_body = doc.add_paragraph(body_body)
+        p_body.paragraph_format.line_spacing_rule = WD_LINE_SPACING.ONE_POINT_FIVE
+
+    # Add valediction
+    p_valediction = doc.add_paragraph(valediction())
+    p_valediction.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
+
+    # Add name of writer
+    p_writer_name = doc.add_paragraph(writer_name())
+    p_writer_name.runs[0].bold = True
+    p_writer_name.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
+
+    # Save Document
+    document_name = f"{sys.argv[1]}_{sys.argv[2]}_{sys.argv[3]}.docx"
+    doc.save(document_name)
 
 
 def date_doc():
@@ -52,12 +102,14 @@ def date_doc():
 
 
 def address():
+    if sys.argv[3].lower() == "committee":
+        return "CONFIDENTIAL\n\nTO THE COMMITTEE MEMBERS\nOF THE LOCAL CONTENT AND LOCAL PARTICIPATION\nCOMMITTEE"
     return "The Managing Director\nVolta River Authority\nElectro-Volta House\nAccra"
 
 
 def reference():
     ref_year = str(date.today().year)[2:]
-    if sys.argv[3] == "committee":
+    if sys.argv[3].lower() == "committee":
         return "REF: EC/LCLP/COM/" + ref_year + "/0.."
     else:
         ref_year = str(date.today().year)[2:]
@@ -73,12 +125,33 @@ def greeting():
 
 
 def title():
+    if sys.argv[3].lower() == "committee":
+        return (
+            "INVITATION TO MEETING FOR LOCAL CONTENT AND\nLOCAL PARTICIPATION COMMITTEE"
+        )
     return "TITLE:"
 
 
-def complimentary_close():
-    body_space = "\n" * 25
-    return f"{body_space}Yours faithfully,\n\n\nIng.Oscar Amonoo-Neizer\n(Executive Secretary)"
+def body():
+    if sys.argv[3].lower() == "committee":
+        date_of_meeting = input(
+            "Date of the meeting (e.g. Tuesday, March  7, 2023 and Wednesday, March 8 2023): "
+        )
+        time_of_meeting = input("Time of the meeting (e.g. 10:00 am): ")
+        return (
+            f"Members are kindly invited to a virtual meeting of the Local Content and Local Participation "
+            f"Committee on {date_of_meeting}, at {time_of_meeting}.\nThe agenda for the meeting is as follows:\n\t1. "
+        )
+    return "..."
+
+
+def valediction():
+    top_space = "\n" * 10
+    return top_space + "Yours faithfully,\n"
+
+
+def writer_name():
+    return "Ing. Oscar Amonoo-Neizer\n(Executive Secretary)"
 
 
 if __name__ == "__main__":
